16e3c2707a09dec6a7e40ae2ea8fdbb5e59aa5c9
Nino Dionson <25104967@usc.edu.ph>
2026-07-15 09:55:31 +0800

    Delete event-ticket-system directory

--- event-ticket-system/config/mail.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('SMTP_HOST', '');      
-define('SMTP_PORT', 587);
-define('SMTP_USER', '');       
-define('SMTP_PASS', '');      
-define('SMTP_FROM', 'no-reply@ticketstub.test');
-define('SMTP_FROM_NAME', 'TicketStub');
--- event-ticket-system/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/includes/auth.php';
-$was_staff = is_staff();
-logout();
-session_start();
-$_SESSION['flash_success'] = 'You have been logged out.';
-header('Location: ' . base_url($was_staff ? '/staff/login.php' : '/login.php'));
-exit;
